1bd68caa0b77033a72f98de4ff8e827ee7bc7d05
Tom Wallin <tom.wallin89@gmail.com>
2019-08-28 14:09:42 +1000

    updated version number for new release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ynapi-indexeng",
-    version="0.0.2",
+    version="0.0.3",
     author="Thomas Wallin",
     author_email="thomas.wallin89@gmail.com",
     description="YNAB API wrapper for python",
